7369530f089a0cf10bfd59d03e6effe2c70ba0d3
Drew Weston <awfiscus@comcast.net>
2021-03-17 19:57:06 -0600

    after adding items to the workout they now are added to the database and appear correctly on the index.html card

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,7 @@ const Workout = require("../models/workout");
 
 router.get("/api/workouts", (req, res) => {
   Workout.find({})
-    .sort({ day: -1 })
+    .sort({ day: +1 })
     .then((dbWorkout) => {
       res.json(dbWorkout);
     })
@@ -12,7 +12,17 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
-router.post("");
+router.post("/api/workouts", (req, res) => {
+  const day = new Date().setDate(new Date().getDate());
+  console.log(day);
+  Workout.create({ day: day, excersises: req.body })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
 router.put("/api/workouts/:id", (req, res) => {
   Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } })
